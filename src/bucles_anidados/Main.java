aa92a8c8a1e4546b34d525a312fcb624610295bc
Leo037 <141924178+Leo037192@users.noreply.github.com>
2023-09-28 08:08:12 -0600

    Add files via upload

---
+++ src/bucles_anidados/Main.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 10; j++) {
+                for (int k = 0; k < 10; k++) {
+                    for (int l = 0; l < 10; l++) {
+                        for (int mn = 0; mn < 10; mn++) {
+                            System.out.print(i != 3 ? i : "E");
+                            System.out.print(j != 3 ? j : "E");
+                            System.out.print(k != 3 ? k : "E");
+                            System.out.print(l != 3 ? l : "E");
+                            System.out.println(mn != 3 ? mn : "E");
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
