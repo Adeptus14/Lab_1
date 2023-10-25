d740d6bef01cb1f722022390c61088314dc9fd12
ZERO-JIFAR <juanignacioarnold@gmail.com>
2023-10-25 17:29:02 -0300

    terminado ejercicio2 de colecciones

--- src/colecciones/ejercicio2.java
+++ src/colecciones/ejercicio2.java
@@ -1,4 +1,38 @@
 package colecciones;
 
+import java.util.Scanner;
+import java.util.ArrayList;
+
 public class ejercicio2 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        ArrayList<Integer> numList = new ArrayList<Integer>();
+
+        // Numeros ingresados por el usuario
+        for (int i = 1; i <= 20; i++) {
+            System.out.print("Ingresa el " + i + "° numero: ");
+            int num = sc.nextInt();
+            numList.add(num);
+        }
+
+        // Mayor y menor de los numeros
+        int max = numList.get(0);
+        int min = numList.get(0);
+        for (int num : numList) {
+            if (num > max) {
+                max = num;
+            }
+            if (num < min) {
+                min = num;
+            }
+        }
+
+        // Rango
+        int range = max - min;
+
+        System.out.println("El mayor numero es: " + max);
+        System.out.println("El menor numero es: " + min);
+        System.out.println("El rango es: " + range);
+    }
 }
